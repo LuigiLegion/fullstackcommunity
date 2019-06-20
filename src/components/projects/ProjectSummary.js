b44ffd7eeefc98a3d59463d02dfbd1c2ff746f87
Tal Luigi <luigi.t.agr@gmail.com>
2019-06-20 15:23:36 -0400

    added dummy data and plugged the store into the dashboard component, passing the dummy data to projectlist and projectsummary to test out the store

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const ProjectSummary = () => {
+const ProjectSummary = ({ project }) => {
   return (
     <div className="card z-depth-0 project-summary">
       <div className="card-content grey-text text-darken-3">
-        <span className="card-title">Come up with Stackathon idea</span>
+        <span className="card-title">{project.title}</span>
         <p>Posted by Tal Luigi</p>
         <p className="grey-text">June 19th, 12:00</p>
       </div>
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,14 @@
-const initialState = {};
+const initialState = {
+  projects: [
+    { id: '1', title: 'Help me find a study buddy', content: 'Pretty please!' },
+    {
+      id: '2',
+      title: 'Help me find someone to complain about my significant other to',
+      content: 'Would you?',
+    },
+    { id: '3', title: 'Help me find a job', content: 'Anytime now...' },
+  ],
+};
 
 const projectReducer = (state = initialState, action) => {
   return state;
