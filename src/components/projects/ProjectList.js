f54c18938e7ff9e20b271f9c06d0f97bc8b7d413
Tal Luigi <luigi.t.agr@gmail.com>
2019-06-20 13:12:51 -0400

    created projectsummary component

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,31 +1,13 @@
 import React from 'react';
+import ProjectSummary from './ProjectSummary';
 
 const ProjectList = () => {
   return (
     <div className="project-list section">
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Come up with Stackathon idea</span>
-          <p>Posted by Tal Luigi</p>
-          <p className="grey-text">June 19th, 12:00</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Started Working on Stackathon</span>
-          <p>Posted by Tal Luigi</p>
-          <p className="grey-text">June 20th, 10:00</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Use create-react-app</span>
-          <p>Posted by Tal Luigi</p>
-          <p className="grey-text">June 20th, 11:00</p>
-        </div>
-      </div>
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
     </div>
   );
 };
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const ProjectSummary = () => {
+  return (
+    <div className="card z-depth-0 project-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title">Come up with Stackathon idea</span>
+        <p>Posted by Tal Luigi</p>
+        <p className="grey-text">June 19th, 12:00</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectSummary;
+
+// <div className="card z-depth-0 project-summary">
+//   <div className="card-content grey-text text-darken-3">
+//     <span className="card-title">Started Working on Stackathon</span>
+//     <p>Posted by Tal Luigi</p>
+//     <p className="grey-text">June 20th, 10:00</p>
+//   </div>
+// </div>
+
+// <div className="card z-depth-0 project-summary">
+//   <div className="card-content grey-text text-darken-3">
+//     <span className="card-title">Use create-react-app</span>
+//     <p>Posted by Tal Luigi</p>
+//     <p className="grey-text">June 20th, 11:00</p>
+//   </div>
+// </div>
