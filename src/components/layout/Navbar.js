16fcb605ebab4da94dc4d5d3b7115a30e69f708d
Tal Luigi <luigi.t.agr@gmail.com>
2019-06-21 14:40:42 -0400

    connected tracking of signed in status using a turnary operator in the navbar component

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,23 +5,23 @@ import { connect } from 'react-redux';
 import SignedInLinks from './SignedInLinks';
 import SignedOutLinks from './SignedOutLinks';
 
-const Navbar = () => {
+const Navbar = props => {
+  const { auth } = props;
+  const curLinks = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <NavLink to="/" className="left brand-logo">
           FullstackCommunity
         </NavLink>
-        <SignedInLinks />
-        <SignedOutLinks />
+        {curLinks}
       </div>
     </nav>
   );
 };
 
-const mapStateToProps = state => {
-  console.log(state);
-  return {};
-};
+const mapStateToProps = state => ({
+  auth: state.firebase.auth,
+});
 
 export default connect(mapStateToProps)(Navbar);
