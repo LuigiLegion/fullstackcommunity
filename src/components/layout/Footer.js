eba669adcb9a25a96bae7afc99c1e0ee88c36d55
Tal Luigi <luigi.t.agr@gmail.com>
2019-06-21 12:32:48 -0400

    created footer component, connected it to app component, and filled it with partner logo and links

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="page-footer grey darken-3">
+      <div className="container">
+        <div className="col l6 s12">
+          <h4 className="white-text center">
+            FullstackCommunity is a Fullstack Academy, Lyft, and Starbucks
+            partnership:
+          </h4>
+          <div className="logos-parent-container">
+            <div className="logo-container">
+              <a
+                className="grey-text text-lighten-3"
+                href="https://www.lyft.com/"
+              >
+                <img
+                  className="partner-logo"
+                  src="https://upload.wikimedia.org/wikipedia/commons/a/a0/Lyft_logo.svg"
+                  alt="Lyft Logo"
+                />
+              </a>
+            </div>
+            <div className="logo-container">
+              <a
+                className="grey-text text-lighten-3"
+                href="https://www.fullstackacademy.com/"
+              >
+                <img
+                  className="fullstack-logo"
+                  src="https://course_report_production.s3.amazonaws.com/rich/rich_files/rich_files/5153/s300/fsa-logo-stacked.png"
+                  alt="Fullstack Logo"
+                />
+              </a>
+            </div>
+            <div className="logo-container">
+              <a
+                className="grey-text text-lighten-3"
+                href="https://www.starbucks.com/responsibility/learn-more/relationships"
+              >
+                <img
+                  className="partner-logo"
+                  src="https://upload.wikimedia.org/wikipedia/en/d/d3/Starbucks_Corporation_Logo_2011.svg"
+                  alt="Starbucks Logo"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="footer-copyright">
+        <div className="container">
+          © 2019 FullstackCommunity
+          <a
+            className="grey-text text-lighten-3 right"
+            href="https://github.com/LuigiLegion"
+          >
+            Contact Us
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
