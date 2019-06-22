cd07686439366e80d0e88073b92963f97d21694a
Tal Luigi <luigi.t.agr@gmail.com>
2019-06-22 08:09:10 -0400

    set up firebase deployment

---
+++ dist/precache-manifest.5dca7c7efd21f70cb2df1f78018222f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b57fd08662df582cf024292cd182b8d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b434ab0d0e87a918fd03",
+    "url": "/static/css/main.443ac6d0.chunk.css"
+  },
+  {
+    "revision": "898cb0125bf71b7687de",
+    "url": "/static/js/2.6416676f.chunk.js"
+  },
+  {
+    "revision": "b434ab0d0e87a918fd03",
+    "url": "/static/js/main.276ec303.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
