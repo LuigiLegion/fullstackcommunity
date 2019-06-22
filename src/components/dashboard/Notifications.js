1b0cba456e5e0ffe85f444e60b9df3e7a55e6848
Tal Luigi <luigi.t.agr@gmail.com>
2019-06-21 20:44:03 -0400

    added new user and new project cloud functions

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const Notifications = () => {
-  return <p>Notifications</p>;
+  return (
+    <div className="section">
+      <div className="card z-depth-0">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Notifications</span>
+          <ul className="notifications">
+            <li>Notification</li>
+            <li>Notification</li>
+            <li>Notification</li>
+            <li>Notification</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Notifications;
