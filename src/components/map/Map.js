581b7de984d67db5077554b6f770c5d0285d4763
Tal Luigi <luigi.t.agr@gmail.com>
2019-06-23 14:26:23 -0400

    refactored starbucks location popup design

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -205,19 +205,25 @@ const Map = ({ auth, users }) => {
               <strong>{selectedStarbucks.name}</strong>
             </div>
             <hr />
-            <a
-              href={`https://www.google.com/maps/dir/?api=1&origin=${curUserLocationName
-                .split(' ')
-                .join(
-                  '+'
-                )}&destination=starbucks+${selectedStarbucks.name
-                .split(' ')
-                .join('+')}&travelmode=transit`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Navigate
-            </a>
+            <div className="navigation-container">
+              <div>
+                <strong>Closes at: 10PM</strong>
+              </div>
+              <br />
+              <a
+                href={`https://www.google.com/maps/dir/?api=1&origin=${curUserLocationName
+                  .split(' ')
+                  .join(
+                    '+'
+                  )}&destination=starbucks+${selectedStarbucks.name
+                  .split(' ')
+                  .join('+')}&travelmode=transit`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Navigate
+              </a>
+            </div>
           </Popup>
         ) : null}
 
