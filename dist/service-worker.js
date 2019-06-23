5f56a5ee0afe997a1ac72a70e13596ad7374a28f
Tal Luigi <luigi.t.agr@gmail.com>
2019-06-23 12:16:05 -0400

    deployed v2

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5dca7c7efd21f70cb2df1f78018222f8.js"
+  "/precache-manifest.b62b61b0e8a87755104a34aa3e3fee4e.js"
 );
 
 self.addEventListener('message', (event) => {
