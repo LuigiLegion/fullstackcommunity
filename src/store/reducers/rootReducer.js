0d97bb9cf618209ddca0be83bf04936a2768f813
Tal Luigi <luigi.t.agr@gmail.com>
2019-07-26 15:57:53 -0400

    plugged eventsReducer into the rootReducer

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -4,12 +4,14 @@ import { firebaseReducer } from 'react-redux-firebase';
 
 import authReducer from './authReducer';
 import projectReducer from './projectReducer';
+import eventsReducer from './eventsReducer';
 
 const rootReducer = combineReducers({
   firestore: firestoreReducer,
   firebase: firebaseReducer,
   auth: authReducer,
   project: projectReducer,
+  events: eventsReducer,
 });
 
 export default rootReducer;
