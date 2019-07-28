c8f3422aa6f7604ee1f370fde5cdcb1faed8bad8
Tal Luigi <luigi.t.agr@gmail.com>
2019-07-28 18:51:40 -0400

    implemented localStorage javascriptCodersEvents property to store all events for said meetup

--- src/store/reducers/eventsReducer.js
+++ src/store/reducers/eventsReducer.js
@@ -23,6 +23,13 @@ export const getEventsThunkCreator = () => {
       );
       // console.log('eventReducer data.results: ', data.results);
       dispatch(gotEventsActionCreator(data.results));
+      console.log('localStorage BEFORE: ', localStorage);
+      localStorage.clear();
+      localStorage.setItem(
+        'javascriptCodersEvents',
+        JSON.stringify(data.results)
+      );
+      console.log('localStorage AFTER: ', localStorage);
     } catch (error) {
       console.error(error);
     }
