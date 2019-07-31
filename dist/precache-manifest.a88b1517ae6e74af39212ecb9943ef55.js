a6ce4476b6b0b82f6a46a52b05cfbcdf8b9309bf
Tal Luigi <luigi.t.agr@gmail.com>
2019-07-31 07:53:33 -0400

    initial commit post deployment

---
+++ dist/precache-manifest.a88b1517ae6e74af39212ecb9943ef55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a48a9a4f6d354afe6b068891e196d5b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a9de294c1ebd0fbd3e7",
+    "url": "/static/css/main.7e0151e0.chunk.css"
+  },
+  {
+    "revision": "5f5b80682da9ee32e2b9",
+    "url": "/static/js/2.8331e32b.chunk.js"
+  },
+  {
+    "revision": "5a9de294c1ebd0fbd3e7",
+    "url": "/static/js/main.0fa9ebea.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
