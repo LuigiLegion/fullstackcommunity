2224b9340e4d26e93228082cab40e8fc496e5f3a
Tal Luigi <luigi.t.agr@gmail.com>
2019-08-01 19:31:07 -0400

    added the freelancer hub to the events pane successfully

--- src/components/dashboard/Events.js
+++ src/components/dashboard/Events.js
@@ -121,6 +121,29 @@ class Events extends Component {
                 </li>
               </ul>
             )}
+            <ul className="notifications">
+              <li>
+                <span className="red-text-color">
+                  <strong>Free working spaces at Freelancers Hub</strong>
+                </span>
+                <div>5 Hanover Square, New York</div>
+                <div className="events-time-and-rsvp-container">
+                  <div className="grey-text note-date events-time-and-rsvp-containee">
+                    8 free work spaces a Month
+                  </div>
+                  <a
+                    className="events-time-and-rsvp-containee"
+                    href="https://freelancershub.nymediacenter.com/member/daypass"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span className="right">
+                      <strong>RSVP</strong>
+                    </span>
+                  </a>
+                </div>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
