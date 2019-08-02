2e59ac45730040d7a5e7853cdf9e3b90a76eda1c
Tal Luigi <luigi.t.agr@gmail.com>
2019-08-01 21:36:11 -0400

    refactored the spaces component from a class component to a functional one

--- src/components/dashboard/Spaces.js
+++ src/components/dashboard/Spaces.js
@@ -1,47 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Spaces extends Component {
-  componentDidMount() {
-    this.props.getEventsThunk();
-  }
-
-  render() {
-    return (
-      <div className="section">
-        <div className="card z-depth-0">
-          <div className="card-content grey-text text-darken-3">
-            <span className="card-title">
-              <strong>Work Spaces</strong>
-            </span>
-            <hr />
-            <ul className="notifications">
-              <li>
-                <span className="red-text-color">
-                  <strong>Free working spaces at Freelancers Hub</strong>
-                </span>
-                <div>30 John Street, Brooklyn</div>
-                <div className="events-time-and-rsvp-container">
-                  <div className="grey-text note-date events-time-and-rsvp-containee">
-                    8 free work spaces a month
-                  </div>
-                  <a
-                    className="events-time-and-rsvp-containee"
-                    href="https://freelancershub.nymediacenter.com/member/daypass"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <span className="right">
-                      <strong>RSVP</strong>
-                    </span>
-                  </a>
+const Spaces = props => {
+  return (
+    <div className="section">
+      <div className="card z-depth-0">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">
+            <strong>Work Spaces</strong>
+          </span>
+          <ul className="notifications">
+            <li>
+              <span className="red-text-color">
+                <strong>Free working spaces at Freelancers Hub</strong>
+              </span>
+              <div>30 John Street, Brooklyn</div>
+              <div className="events-time-and-rsvp-container">
+                <div className="grey-text note-date events-time-and-rsvp-containee">
+                  8 free work spaces a month
                 </div>
-              </li>
-            </ul>
-          </div>
+                <a
+                  className="events-time-and-rsvp-containee"
+                  href="https://freelancershub.nymediacenter.com/member/daypass"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <span className="right">
+                    <strong>RSVP</strong>
+                  </span>
+                </a>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Spaces;
