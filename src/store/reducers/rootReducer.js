67ec00eaba8e6f76ffcf7c279764295ecb97fa2b
Tal Luigi <luigi.t.agr@gmail.com>
2019-08-05 23:31:20 -0400

    added commits reducer to root reducer

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -5,6 +5,7 @@ import { firebaseReducer } from 'react-redux-firebase';
 import authReducer from './authReducer';
 import projectReducer from './projectReducer';
 import eventsReducer from './eventsReducer';
+import commitsReducer from './eventsReducer';
 
 const rootReducer = combineReducers({
   firestore: firestoreReducer,
@@ -12,6 +13,7 @@ const rootReducer = combineReducers({
   auth: authReducer,
   project: projectReducer,
   events: eventsReducer,
+  commits: commitsReducer,
 });
 
 export default rootReducer;
