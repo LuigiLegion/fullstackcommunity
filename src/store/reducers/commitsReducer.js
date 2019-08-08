122b5602fa66155d758e8a7d90b19196d26dbba2
Tal Luigi <luigi.t.agr@gmail.com>
2019-08-07 22:58:49 -0400

    added action creator call and localstorage caching in a settimeout to store commits data

--- src/store/reducers/commitsReducer.js
+++ src/store/reducers/commitsReducer.js
@@ -1,62 +1,62 @@
 // import axios from 'axios';
 import $ from 'jquery';
 
-// // Data Set:
-// const githubUsers = [
-//   // 'marvinody',
-//   // 'mshalam',
-//   // 'sofibee',
-//   // 'TristanWatanabe',
-//   // 'msicil',
-//   // 'dwyfrequency',
-//   // 'SidharthNambiar',
-//   // 'Eutheran',
-//   // 'xMNG',
-//   // 'driver620',
-//   // 'republicofkang',
-//   // 'PeopleMakeCulture',
-//   // 'findkevin',
-//   // 'JorgeAcostaDLP',
-//   // 'jscheinhorn',
-//   // 'Farhuts',
-//   // 'TalRodin',
-//   // 'wilsonleung32',
-//   // 'EricBot89',
-//   // 'malyavka',
-//   // 'constancek',
-//   // 'SunnyChangMei',
-//   'LuigiLegion',
-//   // 'mercedesgm',
-//   // 'xavierolivares',
-//   // 'willgolden5',
-//   // 'tjhelsel',
-//   // 'wassermandh',
-//   // 'alvinjtang',
-//   // 'tluo9713',
-//   // 'amneet954',
-//   // 'fjiang91',
-//   // 'stanley-c-so',
-//   // 'hdoshi2',
-//   // 'dbtracy',
-//   // 'nschefer',
-//   // 'girkonv',
-//   // 'ricardopineda93',
-//   // 'dcnycoder',
-//   // 'sam-peach',
-//   // 'mimi-san',
-//   // 'sriv97',
-//   // 'daphne178',
-//   // 'MistuhMok',
-//   // 'ArnaldWong',
-//   // 'cho-jason',
-//   // 'rickylaufitness',
-//   // 'jkichler',
-//   // 'cmart11',
-//   // 'vivtong',
-//   // 'AlexanderMann2015',
-//   // 'RobertSAdams32',
-//   // 'FakeBarenziah',
-// ];
+// Data Set:
+const githubUsers = [
+  'marvinody',
+  'mshalam',
+  'sofibee',
+  'TristanWatanabe',
+  'msicil',
+  'dwyfrequency',
+  'SidharthNambiar',
+  'Eutheran',
+  'xMNG',
+  'driver620',
+  'republicofkang',
+  'PeopleMakeCulture',
+  'findkevin',
+  'JorgeAcostaDLP',
+  'jscheinhorn',
+  'Farhuts',
+  'TalRodin',
+  'wilsonleung32',
+  'EricBot89',
+  'malyavka',
+  'constancek',
+  'SunnyChangMei',
+  'LuigiLegion',
+  'mercedesgm',
+  'xavierolivares',
+  'willgolden5',
+  'tjhelsel',
+  'wassermandh',
+  'alvinjtang',
+  'tluo9713',
+  'amneet954',
+  'fjiang91',
+  'stanley-c-so',
+  'hdoshi2',
+  'dbtracy',
+  'nschefer',
+  'girkonv',
+  'ricardopineda93',
+  'dcnycoder',
+  'sam-peach',
+  'mimi-san',
+  'sriv97',
+  'daphne178',
+  'MistuhMok',
+  'ArnaldWong',
+  'cho-jason',
+  'rickylaufitness',
+  'jkichler',
+  'cmart11',
+  'vivtong',
+  'AlexanderMann2015',
+  'RobertSAdams32',
+  'FakeBarenziah',
+];
 
 // Initial State
 const initialState = {
@@ -77,123 +77,50 @@ export const gotCommitsActionCreator = commits => ({
 export const getCommitsThunkCreator = () => {
   return async dispatch => {
     try {
-      $.get(
-        `https://cors-anywhere.herokuapp.com/https://github.com/LuigiLegion`,
-        function(response) {
-          // console.log(response);
+      const allCommitsData = [];
 
-          const commitsCount = $(response)
-            .find('h2')
-            .text()
-            .split(' ')[22];
+      githubUsers.forEach(curGithubUser => {
+        $.get(
+          `https://cors-anywhere.herokuapp.com/https://github.com/${curGithubUser}`,
+          function(res) {
+            // console.log('res: ', res);
 
-          console.log(commitsCount);
+            const filtResArr = $(res)
+              .find('h2')
+              .text()
+              .split(' ');
 
-          // const commits = $(response).filter(
-          //   document.querySelector('f4 text-normal mb-2')
-          // );
+            const curGithubUserTotalCommits = filtResArr[22]
+              ? filtResArr[22]
+              : filtResArr[23];
 
-          // console.log(commits);
+            // console.log('commitsCount: ', curGithubUserTotalCommits);
 
-          // const commits = $(response).find('f4 text-normal mb-2');
-          // console.log(commits);
+            const curGithubUserObj = {
+              githubUsername: curGithubUser,
+              totalCommits: curGithubUserTotalCommits,
+            };
 
-          // const commits = response.match(
-          //   /<h2 class="f4 text-normal mb-2">([\d]*) contributions in the last year <\/h2>/
-          // );
+            // console.log('curGithubUserObj: ', curGithubUserObj);
 
-          // console.log(commits);
+            allCommitsData.push(curGithubUserObj);
+          }
+        );
+      });
 
-          // const pattern = `<h2 class="f4 text-normal          mb-2">
-          //       622 contributions
-          //         in the last year
-          //     <\/h2>
-          // `;
+      setTimeout(() => {
+        console.log('allCommitsData: ', allCommitsData);
 
-          // const pattern = `<h4 class="text-gray-light text-normal text-mono f5 mb-2 border-lg-top pt-lg-3">Connect with others<\/h4>`;
+        dispatch(gotCommitsActionCreator(allCommitsData));
 
-          // const commits = response.match(new RegExp(pattern));
+        console.log('commitsReducer localStorage pre-set: ', localStorage);
 
-          // console.log(commits);
+        // localStorage.clear();
 
-          // const commits = response.match(
-          //   /<h4 class="text-gray-light text-normal text-mono f5 mb-2 border-lg-top pt-lg-3">Connect with others<\/h4>/
-          // );
+        localStorage.setItem('commits', JSON.stringify(allCommitsData));
 
-          // console.log(commits);
-        }
-      );
-
-      // const { data } = await axios.get(
-      //   `https://cors-anywhere.herokuapp.com/https://github.com/LuigiLegion`
-      // );
-
-      // console.log(data.querySelector('.f4 text-normal mb-2'));
-
-      // const allCommitsData = [];
-
-      // githubUsers.map(async curGithubUsername => {
-      //   const curGithubUsernameProfileData = await axios.get(
-      //     `https://crossorigin.me/https://github.com/${curGithubUsername}`
-      //   );
-
-      //   console.log(curGithubUsernameProfileData.data);
-      // });
-
-      // githubUsers.map(async curGithubUsername => {
-      //   const curGithubUsernameReposData = await axios.get(
-      //     `https://api.github.com/users/${curGithubUsername}/repos`
-      //   );
-
-      //   console.log(
-      //     'curGithubUsernameRepos: ',
-      //     curGithubUsernameReposData.data
-      //   );
-
-      //   let curGithubUsernameTotalCommits = 0;
-
-      //   curGithubUsernameReposData.data.map(async curRepo => {
-      //     const curAxiosUrl = `https://api.github.com/repos/${curGithubUsername}/${
-      //       curRepo.name
-      //     }/contributors`;
-
-      //     console.log('curAxiosUrl: ', curAxiosUrl);
-
-      //     const curRepoContributorsData = await axios.get(curAxiosUrl);
-
-      //     console.log('curRepoContributorsData: ', curRepoContributorsData);
-
-      //     curRepoContributorsData.data.map(curContributor => {
-      //       if (curContributor.login === curGithubUsername) {
-      //         curGithubUsernameTotalCommits += curContributor.contributions;
-      //       }
-      //       return curGithubUsernameTotalCommits;
-      //     });
-
-      //     console.log(
-      //       'curGithubUsernameTotalCommits: ',
-      //       curGithubUsernameTotalCommits
-      //     );
-      //   });
-
-      //   setTimeout(function() {
-      //     allCommitsData.push({
-      //       githubUsername: curGithubUsername,
-      //       totalCommits: curGithubUsernameTotalCommits,
-      //     });
-
-      //     console.log('allCommitsData: ', allCommitsData);
-      //   }, 2000);
-      // });
-
-      // setTimeout(function() {
-      //   dispatch(gotCommitsActionCreator(allCommitsData));
-
-      //   console.log('commitsReducer localStorage pre-set: ', localStorage);
-      //   // localStorage.clear();
-      //   localStorage.setItem('commits', JSON.stringify(allCommitsData));
-      //   console.log('commitsReducer localStorage post-set: ', localStorage);
-      // }, 10000);
+        console.log('commitsReducer localStorage post-set: ', localStorage);
+      }, 10000);
     } catch (error) {
       console.error(error);
     }
