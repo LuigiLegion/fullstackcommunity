1f8a6f28eca408ba3fbc0678970375c8e0642116
Tal Luigi <luigi.t.agr@gmail.com>
2019-10-03 23:52:15 -0400

    feat: added vueNYC meetups in eventsReducer

--- src/store/reducers/eventsReducer.js
+++ src/store/reducers/eventsReducer.js
@@ -61,7 +61,6 @@ export const getEventsThunkCreator = () => {
       const useReactNycMeetups = await axios.get(
         'https://cors-anywhere.herokuapp.com/https://api.meetup.com/2/events?&sign=true&photo-host=public&group_id=31543338&page=20'
         );
-
       // console.log(
       //   'useReactNycMeetups: ',
       //   useReactNycMeetups.data.results
@@ -70,10 +69,17 @@ export const getEventsThunkCreator = () => {
       const mongodbNycMeetups = await axios.get(
         'https://cors-anywhere.herokuapp.com/https://api.meetup.com/2/events?&sign=true&photo-host=public&group_id=1629296&page=20'
         );
-      
       // console.log(
       //   'mongodbNycMeetups: ',
       //   mongodbNycMeetups.data.results
+      // );
+
+      const vueNycMeetups = await axios.get(
+        'https://cors-anywhere.herokuapp.com/https://api.meetup.com/2/events?&sign=true&photo-host=public&group_id=23275212&page=20'
+        );
+      // console.log(
+      //   'vueNycMeetups: ',
+      //   vueNycMeetups.data.results
       // );
 
       const allMeetupsData = [
@@ -82,6 +88,7 @@ export const getEventsThunkCreator = () => {
         ...reactNycMeetups.data.results,
         ...useReactNycMeetups.data.results,
         ...mongodbNycMeetups.data.results,
+        ...vueNycMeetups.data.results,
       ];
 
       dispatch(gotEventsActionCreator(allMeetupsData));
