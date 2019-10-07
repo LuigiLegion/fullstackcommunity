d6a67c409f7565f8631b9ae34272dab76d300f96
Tal Luigi <luigi.t.agr@gmail.com>
2019-10-07 01:10:27 -0400

    update: added card design to HackerHours component

--- src/components/dashboard/HackerHours.js
+++ src/components/dashboard/HackerHours.js
@@ -2,30 +2,37 @@ import React from 'react';
 
 const HackerHours = props => {
     return (
-        <div>
-            <ul className="notifications">
-                <li>
-                <span className="red-text-color">
-                    <strong>Hacker Hours at Fullstack Academy of Code</strong>
+        <div className="section">
+            <div className="card z-depth-0">
+                <div className="card-content grey-text text-darken-3">
+                <span className="card-title">
+                    <strong>Hacker Hours at Fullstack Academy</strong>
                 </span>
-                <div>5 Hanover Square, New York</div>
-                <div className="events-time-and-rsvp-container">
-                    <div className="grey-text note-date events-time-and-rsvp-containee">
-                    Every second Monday of the month
-                    </div>
-                    <a
-                    className="events-time-and-rsvp-containee"
-                    href="https://www.eventbrite.com/e/hacker-hours-at-fullstack-academy-tickets-63423943723?aff=erelexpmlt"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    >
-                    <span className="right">
-                        <strong>RSVP</strong>
-                    </span>
-                    </a>
+                    <ul className="notifications">
+                        <li>
+                        <span className="red-text-color">
+                            <strong>Hacker Hours at Fullstack Academy of Code</strong>
+                        </span>
+                        <div>5 Hanover Square, New York</div>
+                        <div className="events-time-and-rsvp-container">
+                            <div className="grey-text note-date events-time-and-rsvp-containee">
+                            Every second Monday of the month
+                            </div>
+                            <a
+                            className="events-time-and-rsvp-containee"
+                            href="https://www.eventbrite.com/e/hacker-hours-at-fullstack-academy-tickets-63423943723?aff=erelexpmlt"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            >
+                            <span className="right">
+                                <strong>RSVP</strong>
+                            </span>
+                            </a>
+                        </div>
+                        </li>
+                    </ul>
                 </div>
-                </li>
-            </ul>
+            </div>
         </div>
     )
 }
