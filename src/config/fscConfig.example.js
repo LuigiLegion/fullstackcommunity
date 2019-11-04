398d8bef6eb2421b759e2440acc8110ec45fbb39
Manuel Bugallo <marvinody@gmail.com>
2019-11-04 18:54:01 -0500

    feat: added sample config files

---
+++ src/config/fscConfig.example.js
@@ -0,0 +1,3 @@
+// this is the code that people have to successfully give to signup
+const fullstackCommunityAccessToken = 'ABC easy as 123';
+export default fullstackCommunityAccessToken
---
+++ src/config/mbglConfig.example.js
@@ -0,0 +1,3 @@
+const mapboxAccessToken =
+  'YOUR_MAPBOX_ACCESS_TOKEN';
+export default mapboxAccessToken;
