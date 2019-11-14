b99242a9ec31319d45e466d0c7df50505242bdf2
Tal Luigi <luigi.t.agr@gmail.com>
2019-11-14 12:26:42 -0500

    update: added a red asterisk to all mandatory fields in SignIn component

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -36,11 +36,15 @@ export class SignIn extends Component {
           <form onSubmit={this.handleSubmit} className="card white">
             <h5 className="grey-text text-darken-3">Sign In</h5>
             <div className="input-field">
-              <label htmlFor="email">Email</label>
+              <label htmlFor="email">
+                Email<span className="red-text-color">*</span>
+              </label>
               <input type="email" id="email" onChange={this.handleChange} />
             </div>
             <div className="input-field">
-              <label htmlFor="password">Password</label>
+              <label htmlFor="password">
+                Password<span className="red-text-color">*</span>
+              </label>
               <input
                 type="password"
                 id="password"
