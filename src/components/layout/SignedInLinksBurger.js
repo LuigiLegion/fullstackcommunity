e8cba0ed9c2c95d2f6cf8c2121472d41510a75d6
Tal Luigi <luigi.t.agr@gmail.com>
2019-12-15 06:02:05 -0500

    feat: removed local burgerStyles in the burger components and imported it from the styles file to make the code cleaner

--- src/components/layout/SignedInLinksBurger.js
+++ src/components/layout/SignedInLinksBurger.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { slide as Menu } from 'react-burger-menu';
 
 import { signOutThunkCreator } from '../../store/reducers/authReducer';
+import { burgerStyles } from '../../styles';
 
 class SignedInLinksBurger extends Component {
   constructor() {
@@ -96,50 +97,3 @@ export default connect(
   null,
   mapDispatchToProps
 )(SignedInLinksBurger);
-
-const burgerStyles = {
-  bmBurgerButton: {
-    position: 'fixed',
-    width: '36px',
-    height: '30px',
-    right: '14px',
-    top: '14px',
-  },
-  bmBurgerBars: {
-    background: '#ef5350',
-  },
-  bmBurgerBarsHover: {
-    background: '#a90000',
-  },
-  bmCrossButton: {
-    height: '24px',
-    width: '24px',
-    backgroundColor: '#424242',
-  },
-  bmCross: {
-    background: '#bdc3c7',
-  },
-  bmMenuWrap: {
-    position: 'fixed',
-    height: '100%',
-  },
-  bmMenu: {
-    background: '#373a47',
-    // padding: '2.5em 1.5em 0',
-    fontSize: '1.15em',
-  },
-  bmMorphShape: {
-    fill: '#373a47',
-  },
-  bmItemList: {
-    color: '#b8b7ad',
-    backgroundColor: '#ef5350',
-    padding: '0.8em',
-  },
-  bmItem: {
-    display: 'inline-block',
-  },
-  bmOverlay: {
-    background: 'rgba(0, 0, 0, 0.3)',
-  },
-};
--- src/components/layout/SignedOutLinksBurger.js
+++ src/components/layout/SignedOutLinksBurger.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import { slide as Menu } from 'react-burger-menu';
 
+import { burgerStyles } from '../../styles';
+
 class SignedOutLinksBurger extends Component {
   constructor() {
     super();
@@ -64,50 +66,3 @@ class SignedOutLinksBurger extends Component {
 }
 
 export default SignedOutLinksBurger;
-
-const burgerStyles = {
-  bmBurgerButton: {
-    position: 'fixed',
-    width: '36px',
-    height: '30px',
-    right: '14px',
-    top: '14px',
-  },
-  bmBurgerBars: {
-    background: '#ef5350',
-  },
-  bmBurgerBarsHover: {
-    background: '#a90000',
-  },
-  bmCrossButton: {
-    height: '24px',
-    width: '24px',
-    backgroundColor: '#424242',
-  },
-  bmCross: {
-    background: '#bdc3c7',
-  },
-  bmMenuWrap: {
-    position: 'fixed',
-    height: '100%',
-  },
-  bmMenu: {
-    background: '#373a47',
-    // padding: '2.5em 1.5em 0',
-    fontSize: '1.15em',
-  },
-  bmMorphShape: {
-    fill: '#373a47',
-  },
-  bmItemList: {
-    color: '#b8b7ad',
-    backgroundColor: '#ef5350',
-    padding: '0.8em',
-  },
-  bmItem: {
-    display: 'inline-block',
-  },
-  bmOverlay: {
-    background: 'rgba(0, 0, 0, 0.3)',
-  },
-};
