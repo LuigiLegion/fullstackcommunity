710f41df9780b9d02e2d7be499a6a4c6bc561f1f
Tal Luigi <luigi.t.agr@gmail.com>
2020-03-21 15:31:53 -0400

    feat: refactored the Dashboard component from a class component to a functional component using hooks, utilizing useEffect to only fetch meetups if they have yet to be fetched, as well as adding prop types, code block categories, and proper spacing

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,46 +1,70 @@
-import React, { Component } from 'react';
+// Imports
+import React, { useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { firestoreConnect } from 'react-redux-firebase';
+import PropTypes from 'prop-types';
 
 import ProjectList from '../projects/ProjectList';
 import Notifications from './Notifications';
 import Spaces from './Spaces';
-// import Events from './Events';
+import { getMeetupsThunkCreator } from '../../store/reducers/meetupsReducer';
 
-class Dashboard extends Component {
-  render() {
-    const { auth, projects, notifications } = this.props;
-    if (!auth.uid) {
-      return <Redirect to="/signin" />;
-    } else {
-      return (
-        <div className="dashboard container">
-          <div className="row">
-            <div className="col s12 m6">
-              <ProjectList projects={projects} />
-            </div>
-            <div className="col s12 m5 offset-m1">
-              <Notifications notifications={notifications} />
-              <Spaces />
-              {/* <Events /> */}
-            </div>
+// Component
+const Dashboard = ({
+  auth,
+  projects,
+  notifications,
+  fetchedMeetups,
+  getMeetupsThunk,
+}) => {
+  useEffect(() => {
+    if (!fetchedMeetups) {
+      getMeetupsThunk();
+    }
+  }, [fetchedMeetups, getMeetupsThunk]);
+
+  if (!auth.uid) {
+    return <Redirect to="/signin" />;
+  } else {
+    return (
+      <div className="dashboard container">
+        <div className="row">
+          <div className="col s12 m6">
+            <ProjectList projects={projects} />
+          </div>
+
+          <div className="col s12 m5 offset-m1">
+            <Notifications notifications={notifications} />
+
+            <Spaces />
           </div>
         </div>
-      );
-    }
+      </div>
+    );
   }
-}
+};
 
+// Container
 const mapStateToProps = state => ({
   auth: state.firebase.auth,
   notifications: state.firestore.ordered.notifications,
   projects: state.firestore.ordered.projects,
+  fetchedMeetups: state.meetups.fetchedMeetups,
+});
+
+const mapDispatchToProps = dispatch => ({
+  getMeetupsThunk() {
+    dispatch(getMeetupsThunkCreator());
+  },
 });
 
 export default compose(
-  connect(mapStateToProps),
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  ),
   firestoreConnect([
     {
       collection: 'projects',
@@ -53,3 +77,12 @@ export default compose(
     },
   ])
 )(Dashboard);
+
+// Prop Types
+Dashboard.propTypes = {
+  auth: PropTypes.object,
+  notifications: PropTypes.array,
+  projects: PropTypes.array,
+  fetchedMeetups: PropTypes.bool,
+  getMeetupsThunk: PropTypes.func,
+};
