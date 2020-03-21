f50a6477c78d33308223249621dbae53eebe605d
Tal Luigi <luigi.t.agr@gmail.com>
2020-03-21 15:12:08 -0400

    chore: refactored projectReducer as projectsReducer and added proper spacing

--- src/store/reducers/projectsReducer.js
+++ src/store/reducers/projectsReducer.js
@@ -3,17 +3,17 @@ const initialState = {
   creationError: null,
 };
 
-// Actions
+// Action Types
 const CREATE_PROJECT_SUCCESS = 'CREATE_PROJECT_SUCCESS';
 const CREATE_PROJECT_ERROR = 'CREATE_PROJECT_ERROR';
 
 // Action Creators
-export const createProjectSuccessActionCreator = newProject => ({
+const createProjectSuccessActionCreator = newProject => ({
   type: CREATE_PROJECT_SUCCESS,
   newProject,
 });
 
-export const createProjectErrorActionCreator = error => ({
+const createProjectErrorActionCreator = error => ({
   type: CREATE_PROJECT_ERROR,
   error,
 });
@@ -23,8 +23,10 @@ export const createProjectThunkCreator = newProject => {
   return async (dispatch, getState, { getFirebase, getFirestore }) => {
     try {
       const firestore = getFirestore();
+
       const curAuthorProfile = getState().firebase.profile;
       const curAuthorId = getState().firebase.auth.uid;
+
       await firestore.collection('projects').add({
         ...newProject,
         authorFirstName: curAuthorProfile.firstName,
@@ -33,6 +35,7 @@ export const createProjectThunkCreator = newProject => {
         authorId: curAuthorId,
         createdAt: new Date(),
       });
+
       dispatch(createProjectSuccessActionCreator(newProject));
     } catch (error) {
       console.error(error);
@@ -42,17 +45,19 @@ export const createProjectThunkCreator = newProject => {
 };
 
 // Reducer
-const projectReducer = (state = initialState, action) => {
+const projectsReducer = (state = initialState, action) => {
   switch (action.type) {
     case CREATE_PROJECT_ERROR:
       console.log('Create new project error: ', action.error);
       return { ...state, creationError: action.error };
+
     case CREATE_PROJECT_SUCCESS:
       console.log('Created new project successfully: ', action.newProject);
       return { ...state, creationError: null };
+
     default:
       return state;
   }
 };
 
-export default projectReducer;
+export default projectsReducer;
