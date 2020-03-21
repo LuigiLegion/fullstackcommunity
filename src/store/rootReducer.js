260186cb68eff1ae4a439dfdd59f9ea11b48566d
Tal Luigi <luigi.t.agr@gmail.com>
2020-03-21 15:16:43 -0400

    chore: added code block categories in src/index.js, src/store/index.js, and rootReducer

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,16 +1,18 @@
+// Imports
 import { combineReducers } from 'redux';
 import { firebaseReducer } from 'react-redux-firebase';
 import { firestoreReducer } from 'redux-firestore';
 
 import authReducer from './reducers/authReducer';
-import projectReducer from './reducers/projectReducer';
+import projectsReducer from './reducers/projectsReducer';
 import meetupsReducer from './reducers/meetupsReducer';
 
+// Initializations
 const rootReducer = combineReducers({
   firebase: firebaseReducer,
   firestore: firestoreReducer,
   auth: authReducer,
-  project: projectReducer,
+  projects: projectsReducer,
   meetups: meetupsReducer,
 });
 
