b51935d8beb15043c0b6a84934ab6ca0bcd07ae5
Tal Luigi <luigi.t.agr@gmail.com>
2020-08-26 23:16:54 -0400

    feat: refactored relevant links component rendering conditional logic in the Navbar component

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -31,12 +31,10 @@ const Navbar = ({ auth, profile, isLoading }) => {
   }, [width]);
 
   let curLinks;
-  if (auth.uid) {
-    if (largeViewCheck) {
-      curLinks = <SignedInLinks profile={profile} />;
-    } else {
-      curLinks = <SignedInLinksBurger profile={profile} />;
-    }
+  if (auth.uid && largeViewCheck) {
+    curLinks = <SignedInLinks profile={profile} />;
+  } else if (auth.uid) {
+    curLinks = <SignedInLinksBurger profile={profile} />;
   } else if (largeViewCheck) {
     curLinks = <SignedOutLinks />;
   } else {
@@ -47,7 +45,7 @@ const Navbar = ({ auth, profile, isLoading }) => {
     <div className="navbar-fixed">
       <nav className="nav-wrapper navbar">
         <div>
-          <NavLink to="/" className="left brand-logo navbar-logo">
+          <NavLink to="/" className="left navbar-logo">
             <span className="text-style-bold navbar-text-color">
               {largeViewCheck ? 'Fullstack Community' : 'FSCommunity'}
             </span>
