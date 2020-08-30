66ad37dc87880ca775ab152fe457e8cf2c7b5a6d
Tal Luigi <luigi.t.agr@gmail.com>
2020-08-29 23:55:00 -0400

    chore: removed the /meetups route in App.py

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Dashboard from './components/dashboard/Dashboard';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
 import Map from './components/map/Map';
-import Meetups from './components/meetups/Meetups';
 import CreateProject from './components/projects/CreateProject';
 import SingleProject from './components/projects/SingleProject';
 import PageNotFound from './components/404/PageNotFound';
@@ -24,7 +23,6 @@ const App = () => {
           <Route path="/signin" component={SignIn} />
           <Route path="/signup" component={SignUp} />
           <Route path="/map" component={Map} />
-          <Route path="/meetups" component={Meetups} />
           <Route path="/newproject" component={CreateProject} />
           <Route path="/project/:id" component={SingleProject} />
           <Route path="/:wildcard" component={PageNotFound} />
