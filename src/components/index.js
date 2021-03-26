9f4f91c4ce0e6f6b8769fa935945b1f97e12903f
Tal Luigi <luigi.t.agr@gmail.com>
2021-03-26 06:02:02 -0400

    feat: created the components index file with component export statements to be used as a centralized import location for all components

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+// Exports
+export { default as PageNotFound } from './404/PageNotFound';
+export { default as SignIn } from './auth/SignIn';
+export { default as SignUp } from './auth/SignUp';
+export { default as Dashboard } from './dashboard/Dashboard';
+export { default as Notification } from './dashboard/Notification';
+export { default as Notifications } from './dashboard/Notifications';
+export { default as WorkSpaces } from './dashboard/WorkSpaces';
+export { default as Hello } from './links/Hello';
+export { default as SignedInLinks } from './links/SignedInLinks';
+export { default as SignedInLinksBurger } from './links/SignedInLinksBurger';
+export { default as SignedOutLinks } from './links/SignedOutLinks';
+export { default as SignedOutLinksBurger } from './links/SignedOutLinksBurger';
+export { default as Map } from './map/Map';
+export { default as MapMarker } from './map/MapMarker';
+export { default as Navbar } from './navbar/Navbar';
+export { default as Preloader } from './navbar/Preloader';
+export { default as Project } from './projects/Project';
+export { default as ProjectCreate } from './projects/ProjectCreate';
+export { default as ProjectPreview } from './projects/ProjectPreview';
+export { default as Projects } from './projects/Projects';
