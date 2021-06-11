e7470ff54ed89cc400b15155b155371b74db38c4
Tal Luigi <luigi.t.agr@gmail.com>
2021-06-11 00:10:00 -0400

    feat: refactored the starbucks, whole foods, freelancer hub, aws loft, and campus popup logic into a uniform format and moved it into the MapPopup component

---
+++ src/components/map/MapPopup.js
@@ -0,0 +1,74 @@
+// Imports
+import React from 'react';
+import { Popup } from 'react-map-gl';
+import PropTypes from 'prop-types';
+
+import regexReplace from '../../utils';
+
+// Component
+const MapPopup = ({
+  latitude,
+  longitude,
+  setSelected,
+  type,
+  address,
+  openingHours,
+  rsvpUrl,
+  userLocation,
+}) => {
+  return (
+    <Popup
+      latitude={latitude}
+      longitude={longitude}
+      closeOnClick={false}
+      onClose={() => setSelected(null)}
+    >
+      <div className="text-style-bold location-description">
+        {type} - {address}
+      </div>
+
+      <hr />
+
+      <div className="navigation-container">
+        <div className="text-style-bold navigation-containee">
+          Opening Hours
+        </div>
+
+        <div className="navigation-containee">{openingHours}</div>
+
+        <a href={rsvpUrl} target="_blank" rel="noopener noreferrer">
+          <span className="text-style-bold">RSVP</span>
+        </a>
+
+        <br />
+
+        <a
+          href={`https://www.google.com/maps/dir/?api=1&origin=${regexReplace(
+            userLocation
+          )}+Subway+Station&destination=${regexReplace(type)}+${regexReplace(
+            address
+          )}&travelmode=transit`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span className="text-style-bold">Navigate</span>
+        </a>
+      </div>
+    </Popup>
+  );
+};
+
+// Prop Types
+MapPopup.propTypes = {
+  latitude: PropTypes.number,
+  longitude: PropTypes.number,
+  setSelected: PropTypes.func,
+  type: PropTypes.string,
+  address: PropTypes.string,
+  openingHours: PropTypes.string,
+  rsvpUrl: PropTypes.string,
+  userLocation: PropTypes.string,
+};
+
+// Exports
+export default MapPopup;
