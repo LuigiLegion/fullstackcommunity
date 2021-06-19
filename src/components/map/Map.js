97a8aac1806f1f414fd0cf26aa83f617af5d3e18
Tal Luigi <luigi.t.agr@gmail.com>
2021-06-18 23:57:24 -0400

    feat: refactored the Map component to use the MapPopupLibrary component to render library popups

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -10,7 +10,7 @@ import { compose } from 'redux';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 
-import { MapMarker, MapPopup, MapPopupAlum } from '..';
+import { MapMarker, MapPopup, MapPopupAlum, MapPopupLibrary } from '..';
 import { getMeetupsThunkCreator } from '../../store';
 import { locations as libraries } from '../../data/public-library-locations';
 import { locations as starbucks } from '../../data/starbucks-locations';
@@ -203,105 +203,11 @@ const Map = ({ auth, users, meetups, fetchedMeetups, getMeetupsThunk }) => {
         />
 
         {selectedLibrary ? (
-          <Popup
-            latitude={selectedLibrary.lat}
-            longitude={selectedLibrary.lon}
-            closeOnClick={false}
-            onClose={() => setSelectedLibrary(null)}
-          >
-            <div className="text-style-bold location-description">
-              {selectedLibrary.oversightAgency} - {selectedLibrary.address}
-            </div>
-
-            <hr />
-
-            <div className="navigation-container">
-              <div className="navigation-containee">
-                <span className="text-style-bold">Opening Hours</span>
-
-                <div>
-                  <span className="text-style-bold">Monday: </span>
-                  {selectedLibrary.monOpen} - {selectedLibrary.monClose}
-                  {selectedLibrary.monReopen
-                    ? `, ${selectedLibrary.monReopen}-${selectedLibrary.monReclose}`
-                    : null}
-                </div>
-
-                <div>
-                  <span className="text-style-bold">Tuesday: </span>
-                  {selectedLibrary.tueOpen} - {selectedLibrary.tueClose}
-                  {selectedLibrary.tueReopen
-                    ? `, ${selectedLibrary.tueReopen}-${selectedLibrary.tueReclose}`
-                    : null}
-                </div>
-
-                <div>
-                  <span className="text-style-bold">Wednesday: </span>
-                  {selectedLibrary.wedOpen} - {selectedLibrary.wedClose}
-                  {selectedLibrary.wedReopen
-                    ? `, ${selectedLibrary.wedReopen}-${selectedLibrary.wedReclose}`
-                    : null}
-                </div>
-
-                <div>
-                  <span className="text-style-bold">Thursday: </span>
-                  {selectedLibrary.thuOpen} - {selectedLibrary.thuClose}
-                  {selectedLibrary.thuReopen
-                    ? `, ${selectedLibrary.thuReopen}-${selectedLibrary.thuReclose}`
-                    : null}
-                </div>
-
-                <div>
-                  <span className="text-style-bold">Friday: </span>
-                  {selectedLibrary.friOpen} - {selectedLibrary.friClose}
-                  {selectedLibrary.friReopen
-                    ? `, ${selectedLibrary.friReopen}-${selectedLibrary.friReclose}`
-                    : null}
-                </div>
-
-                <div>
-                  <span className="text-style-bold">Saturday: </span>
-                  {selectedLibrary.satOpen} - {selectedLibrary.satClose}
-                  {selectedLibrary.satReopen
-                    ? `, ${selectedLibrary.satReopen}-${selectedLibrary.satReclose}`
-                    : null}
-                </div>
-
-                <div>
-                  <span className="text-style-bold">Sunday: </span>
-                  {selectedLibrary.sunOpen === 'Closed' ? (
-                    selectedLibrary.sunOpen
-                  ) : (
-                    <div>
-                      {selectedLibrary.sunOpen} - {selectedLibrary.sunClose}
-                      {selectedLibrary.sunReopen
-                        ? `, ${selectedLibrary.sunReopen}-${selectedLibrary.sunReclose}`
-                        : null}
-                    </div>
-                  )}
-                </div>
-              </div>
-
-              <br />
-
-              <a
-                href={`https://www.google.com/maps/dir/?api=1&origin=${userMarker.curUserLocationName.replace(
-                  regex,
-                  '+'
-                )}+Subway+Station&destination=${selectedLibrary.oversightAgency.replace(
-                  regex,
-                  '+'
-                )}+${selectedLibrary.address.replace(
-                  regex,
-                  '+'
-                )}&travelmode=transit`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="text-style-bold">Navigate</span>
-              </a>
-            </div>
-          </Popup>
+          <MapPopupLibrary
+            selectedLibrary={selectedLibrary}
+            setSelectedLibrary={setSelectedLibrary}
+            userLocation={userMarker.curUserLocationName}
+          />
         ) : null}
 
         {selectedStarbucks ? (
