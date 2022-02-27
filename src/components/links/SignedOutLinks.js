768fa7ed09e47ae392415a838d59b21f1635ef24
Tal Luigi <luigi.t.agr@gmail.com>
2022-02-26 23:03:31 -0500

    chore: removed nested span elements across NavLink and anchor elements in the SignedOutLinks component

--- src/components/links/SignedOutLinks.js
+++ src/components/links/SignedOutLinks.js
@@ -7,31 +7,41 @@ const SignedOutLinks = () => {
   return (
     <ul className="right">
       <li className="navbar-link">
-        <NavLink to="/">
-          <span className="navbar-text-color">Hello, guest.</span>
+        <NavLink
+          className="navbar-text-color"
+          to="/"
+        >
+          Hello, guest.
+        </NavLink>
+      </li>
+
+      <li className="navbar-link">
+        <NavLink
+          className="text-style-bold navbar-text-color"
+          to="/signin"
+        >
+          Sign In
+        </NavLink>
+      </li>
+
+      <li className="navbar-link">
+        <NavLink
+          className="text-style-bold navbar-text-color"
+          to="/signup"
+        >
+          Sign Up
         </NavLink>
       </li>
 
       <li className="navbar-link">
         <a
+          className="text-style-bold navbar-text-color padding-right"
           href="https://taluigi.netlify.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <span className="text-style-bold navbar-text-color">Contact</span>
+          Contact
         </a>
-      </li>
-
-      <li className="navbar-link">
-        <NavLink to="/signin">
-          <span className="text-style-bold navbar-text-color">Sign In</span>
-        </NavLink>
-      </li>
-
-      <li className="navbar-link">
-        <NavLink to="/signup">
-          <span className="text-style-bold navbar-text-color">Sign Up</span>
-        </NavLink>
       </li>
     </ul>
   );
