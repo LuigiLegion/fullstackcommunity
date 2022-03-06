f5d77a3a48e9bc54e90a8796f4b4e5dcaf95366a
Tal Luigi <luigi.t.agr@gmail.com>
2022-03-05 23:57:42 -0500

    feat: refactored the store index file to add logic moved from fbConfig, remove logic moved to rootReducer, and support an updated version of firebase and firestore using rrfProps

--- src/store/index.js
+++ src/store/index.js
@@ -1,33 +1,52 @@
 // Imports
-import { composeWithDevTools } from 'redux-devtools-extension';
 import { applyMiddleware, createStore } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import { getFirebase, reactReduxFirebase } from 'react-redux-firebase';
-import { getFirestore, reduxFirestore } from 'redux-firestore';
 import { createLogger } from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import {
+  createFirestoreInstance,
+  reduxFirestore,
+  getFirestore,
+} from 'redux-firestore';
+import { getFirebase } from 'react-redux-firebase';
 
-import firebase from '../config/fbConfig';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+import firebaseConfig from '../config/firebaseConfig';
 import rootReducer from './rootReducer';
 
 // Initializations
+const rrfConfig = {
+  userProfile: 'users',
+  useFirestoreForProfile: true,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore();
+
 const middleware = composeWithDevTools(
   applyMiddleware(
     thunkMiddleware.withExtraArgument({ getFirebase, getFirestore }),
     createLogger({ collapsed: true })
   ),
-  reduxFirestore(firebase),
-  reactReduxFirebase(firebase, {
-    useFirestoreForProfile: true,
-    userProfile: 'users',
-    attachAuthIsReady: true,
-  })
+  reduxFirestore(firebase, firebaseConfig)
 );
 
 const store = createStore(rootReducer, middleware);
 
+export const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance,
+};
+
 // Exports
 export default store;
 export * from './reducers/authReducer';
-export * from './reducers/layoutReducer';
-export * from './reducers/meetupsReducer';
 export * from './reducers/projectsReducer';
+export * from './reducers/meetupsReducer';
+export * from './reducers/layoutReducer';
